d65ea934936ecb37b30f79958b38342b54f17473
lyteabovenyte <lyteabovenyte@gmail.com>
2025-08-17 03:39:21 +0330

    Shutdown struct and it's functionality for shutting down the TCP listener for incomming connections

---
+++ src/shutdown.rs
@@ -0,0 +1,28 @@
+use tokio::sync::broadcast;
+
+struct Shutdown {
+    is_shutdown: bool,
+    notify: broadcast::Receiver<()>,
+}
+
+impl Shutdown {
+    pub(crate) fn new(notify: broadcast::Receiver<()>) -> Self {
+        Shutdown {
+            is_shutdown: false,
+            notify,
+        }
+    }
+
+    pub(crate) fn is_shutdown(&self) -> bool {
+        self.is_shutdown
+    }
+
+    pub(crate) async fn recv(&mut self) {
+        if self.shutdown {
+            return;
+        }
+
+        let _ = self.notify.recv().await;
+        self.is_shutdown = true;
+    }
+}
